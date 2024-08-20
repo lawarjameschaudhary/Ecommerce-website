bba983e426173da57e37244ea9ffa51fe07bc2de
Lawar james chaudhary <lawarjames67929@gmail.com>
2024-08-20 23:53:48 +0545

    small changes

--- src/Components/Cart/CartaCard.jsx
+++ src/Components/Cart/CartaCard.jsx
@@ -21,17 +21,19 @@ const CartCard = (props) => {
         <img className="h-[300px] w-full" src={props.data.thumbnail} />
       </div>
 
-      <div className="p-3 shadow-2xl gap-5 shadow-blue-black">
+      <div className="p-3 shadow-2xl flex flex-col gap-3 shadow-blue-black">
         <div> {props.data.title} </div>
         <div>${props.data.price}</div>
         <div>category: {props.data.category}</div>
 
-        <button
+      <div>
+      <button
           onClick={addtocart}
           className=" px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-md"
         >
           Remove
         </button>
+      </div>
       </div>
     </div>
   );
--- src/Components/ProductSlider/ProductSlider.jsx
+++ src/Components/ProductSlider/ProductSlider.jsx
@@ -3,8 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
 const ProductSlider = () => {
   const settings = {
     dots: true,
@@ -14,21 +12,44 @@ const ProductSlider = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024, // For large screens
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768, // For tablets
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480, // For mobile screens
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <Slider {...settings}>
       <div>
-        <img src="./src/assets/shopping.jpg" alt="Shopping" className="w-full" />
+        <img src="./src/assets/shopping.jpg" alt="Shopping" className="w-full h-auto object-cover" />
       </div>
       <div>
-        <img src="./src/assets/shopping2.jpg" alt="Another" className="w-full" />
+        <img src="./src/assets/shopping2.jpg" alt="Another" className="w-full h-auto object-cover" />
       </div>
       <div>
-        <img src="./src/assets/shopping3.jpg" alt="Another" className="w-full" />
+        <img src="./src/assets/shopping3.jpg" alt="Another" className="w-full h-auto object-cover" />
       </div>
       <div>
-        <img src="./src/assets/shopping4.jpg" alt="Another" className="w-full" />
+        <img src="./src/assets/shopping4.jpg" alt="Another" className="w-full h-auto object-cover" />
       </div>
     </Slider>
   );
